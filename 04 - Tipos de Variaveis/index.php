cb4321f379c060616ab9eaf239eb4d009b373b10
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-03-03 22:51:26 -0300

    Tipos de variáveis

---
+++ 04 - Tipos de Variaveis/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    /*String*/
+    $nome = 'Marcos Newton';
+
+    /*Número ou Inteiros*/
+    $idade = 30;
+
+    /*Concatenar string com variáveis*/
+    echo 'Meu nome é ' .$nome. ' e minha idade é de ' .$idade. ' anos.';
+
+    /*Boolean - Verdadeiro ou Falso*/
+    $bool = true;  // 1
+    $bool = false; // 0
+
+    /*Doubles - Números com casas decimais*/
+    $double = 10.09;
+    echo '<br>'.'Aqui temos um exemplo de número com casas decimais: ' .$double;
+    
+?>
