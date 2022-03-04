f7a718dceb0825767311dbcf38457a3bed8cbe78
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-03-04 14:18:49 -0300

    Definição de constante

---
+++ 05 - Constantes/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nome = 'Marcos'; // Variáveis, este valor pode ser alterado a qualquer momento.
+    echo $nome;
+
+    define('ESPOSA','Camila'); // Definição de uma constante, não pode ser alterado uma vez atribuida.
+    echo ESPOSA;
+
+?>
