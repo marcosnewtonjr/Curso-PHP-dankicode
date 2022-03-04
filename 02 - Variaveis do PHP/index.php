c835828a9704081f49075a47410db67084daf3fa
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-03-03 22:09:22 -0300

    Variáveis padrão

---
+++ 02 - Variaveis do PHP/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    // echo $_SERVER['SERVER_NAME']
+    // echo $_SERVER['DOCUMENT_ROOT']
+    echo '<pre>'; // Organizar sequencia de variáveis na tela
+    print_r($_SERVER); // Este comando trás todas as informações de uma variável
+    echo '</pre>';
+
+?>
