39044a467c2f1efc23556543061d1a1435d00427
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-03-04 15:09:09 -0300

    Operações matemáticas

---
+++ 08 - Operacoes Matematicas/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    $numero = 5 + 5;
+    
+    $numero2 = 10 - 4;
+
+    $resultado = $numero + $numero2;
+
+    echo $resultado;
+
+    /*
+
+    Como toda linguagem de programação temos os seguintes operadores:
+    
+    (+) adição
+    (-) subtração
+    (*) multiplicação
+    (/) divisão
+    (%) resto da divisão
+    (**) potência
+
+    */
+
+?>
