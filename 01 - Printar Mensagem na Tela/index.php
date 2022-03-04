99f87f4793d1c9a64c01a2d9bcce6a588b3032e3
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-03-03 21:58:51 -0300

    Exibindo mensagem na tela

---
+++ 01 - Printar Mensagem na Tela/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha página de testes</title>
+</head>
+<body>
+    <?php
+        echo 'Olá Mundo!' // Comando utilizado para exibir uma mensagem na tela
+    ?>
+</body>
+</html>
