389ef3aa5efc1ee25210fc4d027ddba459968157
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-03-04 15:55:37 -0300

    Operadores matemáticos e logicos

---
+++ 11 - Operadores Matematicos e Logicos/index.php
@@ -0,0 +1,38 @@
+<?php
+
+    $numero1 = 10;
+    $numero2 =11;
+
+    // Operadores matemáticos
+
+    if ($numero1 < $numero2) {
+        // Execute esta ação
+    }
+
+    if ($numero1 <= $numero2) {
+        // Excute esta ação
+    }
+
+    if ($numero1 > $numero2) {
+        // Execute esta ação
+    }
+
+    if ($numero1 >= $numero2) {
+        // Excute esta ação
+    }
+
+    // Operadores lógicos
+
+    $var1 = 'valor1';
+    $var2 = 'valor2';
+    $var3 = 'valor3';
+
+    if (($var1 == $var2) && ($var2 == $var3)) {
+        // Executar esta ação para logica 'E'
+    }
+
+    if (($var1 == $var2) || ($var2 == $var3)) {
+        // Executar esta ação para logica 'OU'
+    }
+
+?>
