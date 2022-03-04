e0fb99dc0a5a3b680750bd6d9b827c6e1e19da70
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-03-03 22:27:32 -0300

    Criando variáveis

---
+++ 03 - Criando Variaveis/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    // No PHP utilizamos $ para definir variáveis
+
+    $nome = 'Marcos';
+    echo 'Meu nome é: ';
+    echo $nome;
+    $nome = 'Aylla';
+    echo '<br>';
+    echo 'Meu nome é: ';
+    echo $nome;
+
+    $idade = 30; // Aqui temos um número
+    $altura = '1,80'; // Aqui temos uma string pois foi atribuida dentro de aspas
+
+?>
