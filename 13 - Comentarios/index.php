a7bb23275e644a1ad1745c8e3c607800e4470276
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-03-04 16:31:30 -0300

    Comentários

--- 13 - Comentarios/index.php
+++ 13 - Comentarios/index.php
@@ -2,9 +2,11 @@
 
     // Comentários não são lidos pelo código
 
+    # Mesmo que tenhamos trechos de código
+
     /*
     Temos os comenários de uma linha
     E também os comentários de multiplas linhas como este
     */
-    
+
 ?>
