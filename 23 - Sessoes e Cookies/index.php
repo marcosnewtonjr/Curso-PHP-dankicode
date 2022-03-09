c505cf4ce67e0940caf71935ec8dcf3e404b954a
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-03-09 14:01:11 -0300

    Iniciando sessões e cookies

---
+++ 23 - Sessoes e Cookies/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Sessões, são dados armazenados em variáveis que são destruidos após o usuário fechar o navegador
+
+    # session_start();
+    # $_SESSION['nome'] = 'Marcos';
+
+    // Cookies, são dados armazenados em variáveis que são destruidos quando determinado pelo programador
+
+    # setcookie('nome','Marcos',time() + 60,'/');
+
+    # echo $_COOKIE['nome']
+
+?>
